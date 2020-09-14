84168ec2550561bed95490fb5e26b2b8005b293c
youri <a;sldkfjfjdksla>
2020-09-14 22:47:42 +0200

    bijna af

--- 08_Boter-Kaas-Eieren/Form1.Designer.cs
+++ 08_Boter-Kaas-Eieren/Form1.Designer.cs
@@ -28,21 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.A0 = new System.Windows.Forms.Button();
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B0 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C0 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // A0
+            // 
+            this.A0.Location = new System.Drawing.Point(206, 279);
+            this.A0.Name = "A0";
+            this.A0.Size = new System.Drawing.Size(70, 70);
+            this.A0.TabIndex = 0;
+            this.A0.UseVisualStyleBackColor = true;
+            this.A0.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // A1
+            // 
+            this.A1.Location = new System.Drawing.Point(282, 279);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(70, 70);
+            this.A1.TabIndex = 1;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // A2
+            // 
+            this.A2.Location = new System.Drawing.Point(358, 279);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(70, 70);
+            this.A2.TabIndex = 2;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(358, 203);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(70, 70);
+            this.B2.TabIndex = 5;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(282, 203);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(70, 70);
+            this.B1.TabIndex = 4;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // B0
+            // 
+            this.B0.Location = new System.Drawing.Point(206, 203);
+            this.B0.Name = "B0";
+            this.B0.Size = new System.Drawing.Size(70, 70);
+            this.B0.TabIndex = 3;
+            this.B0.UseVisualStyleBackColor = true;
+            this.B0.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // C2
+            // 
+            this.C2.Location = new System.Drawing.Point(358, 127);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(70, 70);
+            this.C2.TabIndex = 8;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // C1
+            // 
+            this.C1.Location = new System.Drawing.Point(282, 127);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(70, 70);
+            this.C1.TabIndex = 7;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // C0
+            // 
+            this.C0.Location = new System.Drawing.Point(206, 127);
+            this.C0.Name = "C0";
+            this.C0.Size = new System.Drawing.Size(70, 70);
+            this.C0.TabIndex = 6;
+            this.C0.UseVisualStyleBackColor = true;
+            this.C0.Click += new System.EventHandler(this.Button_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.C0);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.B0);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.A0);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Boter-Kaas-Eieren";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button A0;
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B0;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C0;
     }
 }
 
--- 08_Boter-Kaas-Eieren/Form1.cs
+++ 08_Boter-Kaas-Eieren/Form1.cs
@@ -1,30 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace _08_Boter_Kaas_Eieren
 {
     public partial class Form1 : Form
     {
-        int turn = 0;
-        int[,] magic = new int[3, 3] { { 4, 9, 2 }, { 3, 5, 7 }, { 8, 1, 6 } };
-        int[,] boards = new int[3, 3] { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0 ,0 } };
+        int turn_count = 0;
+
+        int[][] magic = new int[][]
+        {
+            new int[] { 4, 9, 2 },
+            new int[] { 3, 5, 7 },
+            new int[] { 8, 1, 6 } 
+        };
+
+        int[][] boards = new int[][]
+        {
+            new int[] { 0, 0, 0 },
+            new int[] { 0, 0, 0 },
+            new int[] { 0, 0, 0 }
+        };
 
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void checkWin()
-        {
-            // if boards(horz, vert, diag).any == 15 p1 won
-            // if boards(horz, vert, diag).any == 30 p2 won
         }
 
         // priv void position_Click
@@ -32,14 +31,77 @@ namespace _08_Boter_Kaas_Eieren
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            for (int i = 0; i < magic.GetLength(0); i++)
+            for (int i = 0; i < magic[0].GetLength(0); i++)
             {
-                for (int j = 0; j < magic.GetLength(1) ; j++)
+                for (int j = 0; j < magic[1].GetLength(0); j++)
                 {
-                    Console.WriteLine($"{i}{j} - {magic[i, j]} \t {i}{j} - {boards[i,j]}");
+                    Console.WriteLine($"{i}{j} - {magic[i][j]} \t {i}{j} - {boards[i][j]}");
                 }
             }
             
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            int row = b.Name[0] - 65;
+            int column = b.Name[1] - 48;
+
+            if (turn_count % 2 == 0)
+            {
+                boards[row][column] = magic[row][column];
+                b.Text = "X";
+            }
+            else
+            {
+                b.Text = "O";
+            }
+
+            CheckWin();
+            //Check_Row(b.Name);
+
+            Console.WriteLine($"{row} {column}");
+            Print_Board();
+
+            turn_count++;
+        }
+
+        private void Print_Board()
+        {
+            for (int i = 0; i < magic[0].GetLength(0); i++)
+            {
+                for (int j = 0; j < magic[1].GetLength(0); j++)
+                {
+                    Console.WriteLine($"{i}{j} - {magic[i][j]} \t {i}{j} - {boards[i][j]}");
+                }
+            }
+        }
+
+        private void Check_Row(string direction)
+        {
+            foreach (char chr in direction)
+            {
+                switch (chr)
+                {
+                    case 'A': Console.WriteLine("bottom"); break;
+                    case 'B': Console.WriteLine("middle"); break;
+                    case 'C': Console.WriteLine("top"); break;
+                    case '0': Console.WriteLine("first"); break;
+                    case '1': Console.WriteLine("middle"); break;
+                    case '2': Console.WriteLine("last"); break;
+                    default: Console.WriteLine("default"); break;
+                }
+            }
+        }
+
+        private void CheckWin()
+        {
+            if (A0.Text == A1.Text && A1.Text == A2.Text)       // horizontal
+            {
+                Console.WriteLine($"A WONNER! + {A0.Text} + {A1.Text} + {A2.Text}");
+            }
+            // if boards(horz, vert, diag).any == 15 p1 won
+            // if boards(horz, vert, diag).any == 30 p2 won
         }
     }
 }
